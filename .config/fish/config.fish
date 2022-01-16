457a8e14263adeed5dbdac6649b1d4900e0aff39
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-01-15 23:18:46 -0600

    Added gitconfig and cleanup fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,6 +23,6 @@ set MYHOME "/home/david"
 set GIT_DIR "$MYHOME/git"
 set PROGRAMS_DIR "$MYHOME/programs"
 set HOMEBREW_BIN_DIR "/home/linuxbrew/.linuxbrew/bin"
-set PATH "$PATH:$HOMEBREW_BIN_DIR:$MYHOME/.cargo/bin:$MYHOME/scripts:$PROGRAMS_DIR:$PROGRAMS_DIR/clion-2020.3/bin:$PROGRAMS_DIR/zls:$PROGRAMS_DIR/zig-master:$PROGRAMS_DIRiler/zig-cache/bin:/opt/intel/oneapi/vtune/latest/bin64:$PROGRAMS_DIR/shaderc/bin:$PROGRAMS_DIR/vk-zig-gen:$PROGRAMS_DIR/gyro-0.3.0/bin:$GIT_DIR/zls/zig-out/bin"
+set PATH "$PATH:$HOMEBREW_BIN_DIR:$MYHOME/.cargo/bin:$PROGRAMS_DIR:$GIT_DIR/zls/zig-out/bin:$GIT_DIR/gf"
 
 set ZIG_DIR "$PROGRAMS_DIR/zig-master"
