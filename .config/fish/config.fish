cbb34e67a2d898e92f358bc1c3b52bbc76173a8a
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-02-19 20:43:17 -0600

    Fix fish config and add GDB init file

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,12 +18,10 @@ function cdir
     command mkdir $argv && cd $argv
 end
 
-set MYHOME "/home/david"
-
-set GIT_DIR "$MYHOME/git"
-set PROGRAMS_DIR "$MYHOME/programs"
+set GIT_DIR "$HOME/git"
+set PROGRAMS_DIR "$HOME/programs"
 set HOMEBREW_BIN_DIR "/home/linuxbrew/.linuxbrew/bin"
 set MAC_HOMEBREW_BIN_DIR "/opt/homebrew/bin"
 set ZIG_DIR "$PROGRAMS_DIR/zig-master"
-set PATH "$PATH:$HOMEBREW_BIN_DIR:$MYHOME/.cargo/bin:$PROGRAMS_DIR:$GIT_DIR/zls/zig-out/bin:$GIT_DIR/gf:$ZIG_DIR"
+set PATH "$PATH:$HOMEBREW_BIN_DIR:$HOME/.cargo/bin:$PROGRAMS_DIR:$GIT_DIR/zls/zig-out/bin:$GIT_DIR/gf:$ZIG_DIR:$MAC_HOMEBREW_BIN_DIR"
 
