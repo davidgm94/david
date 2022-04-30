687042748bb7f69c9a3da217ef40b8fee9753c74
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-04-30 18:57:50 -0500

    Reorganize install scripts

---
+++ .3_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# These commands should be run after the environment variables are set
+
+# Quit the terminal
+brew install neovim ripgrep bat qemu zig
+# Twice because the first one might fail
+brew install neovim ripgrep bat qemu zig
+
+git clone --recurse-submodules git@github.com:davidgm94/zls.git /home/david/dev/zls
+cd /home/david/dev/zls
+git checkout 0.9.0
+zig build -Drelease-fast
+
+# Install kityy as the default terminal emulator
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 500
