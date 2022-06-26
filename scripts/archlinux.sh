3a2faea319213dc4b295a9b771a7e9be1357be50
David Gonzalez Martin <davidgm94.work@protonmail.com>
2022-06-26 13:28:32 +0200

    More WM integration

--- scripts/archlinux.sh
+++ scripts/archlinux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -S git firefox telegram-desktop signal-desktop lm_sensors htop alacritty kitty wezterm discord gdb neovim ripgrep bat qemu qemu-arch-extra rust-analyzer rustup mpv vlc smplayer nodejs tokei nushell bash-completion libreoffice xorriso llvm cmake ttf-fira-mono ttf-fira-code ttf-fira-sans ttf-dejavu ttf-liberation noto-fonts man i3 i3-wm i3lock i3blocks i3status
+sudo pacman -S git firefox telegram-desktop signal-desktop lm_sensors htop alacritty kitty wezterm discord gdb neovim ripgrep bat qemu qemu-arch-extra rust-analyzer rustup mpv vlc smplayer nodejs tokei nushell bash-completion libreoffice xorriso llvm cmake ttf-fira-mono ttf-fira-code ttf-fira-sans ttf-dejavu ttf-liberation noto-fonts man i3 i3-wm i3lock i3blocks i3status dmenu nvidia-settings pavucontrol xclip lld clang
 
 rustup toolchain install stable
 
